8cb8d8ca43dceef3b448ae79ec62ba7d9d5b1009
ssartell <ssartell@rightpoint.com>
2016-12-04 10:06:22 -0600

    day 3 part 2

--- day03/part1.js
+++ day03/part1.js
@@ -1,20 +1,12 @@
 var R = require('ramda');
 
-var trace = (x) => {
-    console.log(x);
-    return x;
-}
+var isTriangle = (sides) => {
+    return (sides[0] + sides[1] > sides[2])
+        && (sides[1] + sides[2] > sides[0])
+        && (sides[2] + sides[0] > sides[1]);
+};
 
 var readTriangle = R.compose(R.map(parseInt), R.tail, RegExp.prototype.exec.bind(/\s*(\d*)\s*(\d*)\s*(\d*)/), R.trim);
-
-var isTriangle = (sides) => {
-    return ((sides[0] + sides[1] > sides[2])
-        && (sides[1] + sides[2] > sides[0])
-        && (sides[2] + sides[0] > sides[1]))
-        ? 1
-        : 0;
-}
-
 var parseInput = R.compose(R.sum, R.map(R.compose(isTriangle, readTriangle)), R.split('\n'), R.trim)
 
 var solution = R.compose(parseInput);
--- day03/part2.js
+++ day03/part2.js
@@ -1,5 +1,15 @@
 var R = require('ramda');
 
-var solution = (input) => null;
+var isTriangle = (sides) => {
+    return (sides[0] + sides[1] > sides[2])
+        && (sides[1] + sides[2] > sides[0])
+        && (sides[2] + sides[0] > sides[1]);
+};
+
+var readTriangle = R.compose(R.map(parseInt), R.tail, RegExp.prototype.exec.bind(/\s*(\d*)\s*(\d*)\s*(\d*)/), R.trim);
+var toVerticalTriangles = R.compose(R.chain(R.transpose), R.splitEvery(3));
+var parseInput = R.compose(R.sum, R.map(isTriangle), toVerticalTriangles, R.map(readTriangle), R.split('\n'), R.trim)
+
+var solution = R.compose(parseInput);
 
 module.exports = solution;
