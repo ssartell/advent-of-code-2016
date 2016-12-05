de16d525b8ed3ffa12aecdce549e76ecee472561
ssartell <ssartell@rightpoint.com>
2016-12-04 22:16:30 -0600

    clean up day 4

--- day04/part1.js
+++ day04/part1.js
@@ -2,16 +2,16 @@ var R = require('ramda');
 
 var byLengthDesc = R.comparator((a,b) => a.length > b.length);
 var byValue = R.comparator((a,b) => a < b);
-var getCheckSum = R.pipe(R.groupBy(R.identity), R.values, R.sort(R.either(byLengthDesc, byValue)), R.map(R.head), R.take(5), R.join(''));
+var getCheckSum = R.pipe(R.replace(/-/g, ''), R.groupBy(R.identity), R.values, R.sort(R.either(byLengthDesc, byValue)), R.map(R.head), R.take(5), R.join(''));
 
-var toRoomCode = (values) => ({
-        realChecksum: getCheckSum(values[0]),
-        sectorId: parseInt(values[1]),
-        checksum: values[2]
+var toRoom = (name, sectorId, checksum) => ({
+        realChecksum: getCheckSum(name),
+        sectorId: sectorId,
+        checksum: checksum
     });
 
 var checksumsMatch = R.converge(R.equals, [R.prop('realChecksum'), R.prop('checksum')]); 
-var readLine = R.pipe(R.trim, R.match(/((?:[a-z]*-)*)(\d*)\[([a-z]*)\]/), R.tail, R.map(R.replace(/-/g, '')), toRoomCode);
+var readLine = R.pipe(R.trim, R.match(/((?:[a-z]*-?)*)-(\d*)\[([a-z]*)\]/), R.tail, R.adjust(parseInt, 1), R.apply(toRoom));
 var parseInput = R.pipe(R.trim, R.split('\n'), R.map(readLine));
 var solution = R.pipe(parseInput, R.filter(checksumsMatch), R.map(R.prop('sectorId')), R.sum);
 
--- day04/part2.js
+++ day04/part2.js
@@ -2,23 +2,18 @@ var R = require('ramda');
 
 var getCharCode = x => x.charCodeAt(0);
 var toChar = String.fromCharCode;
-var rotate = R.curry((sectorId, x) => {
-    return x === '-' ? ' ' : toChar(((getCharCode(x) - 97 + sectorId) % 26) + 97);
-});
+var rotate = R.curry((sectorId, x) => x === '-' ? ' ' : toChar(((getCharCode(x) - 97 + sectorId) % 26) + 97));
+var cipher = (code, sectorId) => R.pipe(R.map(rotate(sectorId)), R.join(''))(code);
 
-var cipher = (code, sectorId) => {
-    return R.join('', R.map(rotate(sectorId), code));
-};
-
-var toRoomCode = (values) => ({
-        roomName: cipher(values[0], parseInt(values[1])),
-        sectorId: parseInt(values[1]),
+var toRoom = (name, sectorId) => ({
+        name: cipher(name, sectorId),
+        sectorId: sectorId,
     });
 
 var checksumsMatch = R.converge(R.equals, [R.prop('realChecksum'), R.prop('checksum')]);
-var readLine = R.pipe(R.trim, R.match(/((?:[a-z]*-?)*)-(\d*)\[([a-z]*)\]/), R.tail, toRoomCode);
+var readLine = R.pipe(R.trim, R.match(/((?:[a-z]*-?)*)-(\d*)\[([a-z]*)\]/), R.tail, R.adjust(parseInt, 1), R.apply(toRoom));
 var parseInput = R.pipe(R.trim, R.split('\n'), R.map(readLine));
-var matchesName = R.pipe(R.prop('roomName'), R.equals('northpole object storage'));
+var matchesName = R.pipe(R.prop('name'), R.equals('northpole object storage'));
 var solution = R.pipe(parseInput, R.filter(checksumsMatch), R.find(matchesName), R.prop('sectorId'));
 
 module.exports = solution;
