4ad11339c8eb06d068504a69553f4cdde6e27169
ssartell <ssartell@rightpoint.com>
2016-12-05 10:17:14 -0600

    update

--- day04/part2.js
+++ day04/part2.js
@@ -1,9 +1,9 @@
 var R = require('ramda');
 
-var rotate = R.curry((sectorId, x) => x === '-' ? ' ' : String.fromCharCode(((x.charCodeAt(0) - 97 + sectorId) % 26) + 97));
-var cipher = (code, sectorId) => R.pipe(R.map(rotate(sectorId)), R.join(''))(code);
-var toRoom = (name, sectorId) => ({ name: cipher(name, sectorId), sectorId: sectorId });
-var readLine = R.pipe(R.trim, R.match(/((?:[a-z]*-?)*)-(\d*)\[([a-z]*)\]/), R.tail, R.adjust(parseInt, 1), R.apply(toRoom));
+var rotate = R.curry((id, x) => x == '-' ? ' ' : String.fromCharCode(((x.charCodeAt(0) - 97 + id) % 26) + 97));
+var cipher = (name, id) => R.pipe(R.map(rotate(id)), R.join(''))(name);
+var toRoom = (name, id) => ({ name: cipher(name, id), sectorId: id });
+var readLine = R.pipe(R.trim, R.match(/((?:[a-z]*-?)*)-(\d*)/), R.tail, R.adjust(parseInt, 1), R.apply(toRoom));
 var parseInput = R.pipe(R.trim, R.split('\n'), R.map(readLine));
 var matchesName = R.pipe(R.prop('name'), R.equals('northpole object storage'));
 var solution = R.pipe(parseInput, R.find(matchesName), R.prop('sectorId'));
