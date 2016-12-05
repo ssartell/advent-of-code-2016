b37ab01b1244781f5baf2d1552bc48025d37c22e
ssartell <ssartell@rightpoint.com>
2016-12-05 10:12:21 -0600

    short

--- day04/part2.js
+++ day04/part2.js
@@ -2,12 +2,7 @@ var R = require('ramda');
 
 var rotate = R.curry((sectorId, x) => x === '-' ? ' ' : String.fromCharCode(((x.charCodeAt(0) - 97 + sectorId) % 26) + 97));
 var cipher = (code, sectorId) => R.pipe(R.map(rotate(sectorId)), R.join(''))(code);
-
-var toRoom = (name, sectorId) => ({
-        name: cipher(name, sectorId),
-        sectorId: sectorId,
-    });
-
+var toRoom = (name, sectorId) => ({ name: cipher(name, sectorId), sectorId: sectorId });
 var readLine = R.pipe(R.trim, R.match(/((?:[a-z]*-?)*)-(\d*)\[([a-z]*)\]/), R.tail, R.adjust(parseInt, 1), R.apply(toRoom));
 var parseInput = R.pipe(R.trim, R.split('\n'), R.map(readLine));
 var matchesName = R.pipe(R.prop('name'), R.equals('northpole object storage'));
