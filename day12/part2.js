028fcdae68c8a4ffea4e74e332b8daaff8dffcb1
ssartell <ssartell@rightpoint.com>
2016-12-12 12:58:31 -0600

    day 12

---
+++ day12/part2.js
@@ -0,0 +1,60 @@
+var R = require('ramda');
+
+var registers = {
+    a: 0,
+    b: 0,
+    c: 1,
+    d: 0
+};
+
+var isRegister = x => !isFinite(x);
+var valueOrRegister = x => isRegister(x) ? registers[x] : x;
+
+var tryParseInt = x => {
+    var int = parseInt(x);
+    if (isNaN(int)) {
+        return x;
+    } else {
+        return int;
+    }
+};
+
+var fields = ['inst', 'x', 'y'];
+var parseInput = R.pipe(R.trim, R.split('\n'), R.map(R.pipe(R.trim, R.match(/(\w*) (\w*)(?: (-?\w*))?/), R.tail, R.adjust(tryParseInt, 1), R.adjust(tryParseInt, 2), R.zipObj(fields))));
+
+var exec = code => {
+    var i = 0;
+
+    var funcs = {
+        cpy: inst => { 
+            registers[inst.y] = valueOrRegister(inst.x);
+            i++;
+        },
+        inc: inst => {
+            registers[inst.x]++;
+            i++;
+        },
+        dec: inst => {
+            registers[inst.x]--;
+            i++;
+        },
+        jnz: inst => {
+            if (valueOrRegister(inst.x) !== 0) {
+                i += inst.y;
+            } else {
+                i++;
+            }                
+        },
+    };
+
+    while (0 <= i && i < code.length) {
+        var line = code[i];
+        funcs[line.inst](line);
+    }
+
+    return registers['a'];
+}
+
+var solution = R.pipe(parseInput, exec);
+
+module.exports = solution;
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function run(day, part) {
 	console.log('day ' + day + ', part ' + part + ': ' + solution(input));
 }
 
-var day = 11;
+var day = 12;
 // run(day, 1);
 run(day, 2);
 
