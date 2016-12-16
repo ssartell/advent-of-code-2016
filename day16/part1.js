4a5d22f4dd73dcec6cef5caeae624e6896c7041a
ssartell <ssartell@rightpoint.com>
2016-12-16 11:06:30 -0600

    day 16

--- day16/part1.js
+++ day16/part1.js
@@ -5,9 +5,7 @@ var parseInput = R.pipe(R.trim);
 var not = x => x === '0' ? '1' : '0';
 var mirror = R.pipe(R.split(''), R.reverse, R.map(not), R.join(''));
 var dragonStep = x => x + '0' + mirror(x);
-var isEven = x => {
-    return x.length % 2 === 0;
-};
+var isEven = x => x.length % 2 === 0;
 var fixPair = x => x[0] === x[1] ? '1' : '0';
 var checkSum = R.pipe(R.splitEvery(2), R.map(fixPair), R.join(''));
 
