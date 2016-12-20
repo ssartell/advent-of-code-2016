982f4248022095008501c3877426addac594f5bb
ssartell <ssartell@rightpoint.com>
2016-12-20 09:13:59 -0600

    day20

--- day20/part1.js
+++ day20/part1.js
@@ -5,7 +5,6 @@ var parseInput = R.pipe(R.trim, R.split('\n'), R.map(R.pipe(R.split('-'), R.map(
 var rangeContains = R.curry((n, r) => r[0] <= n && n <= r[1]);
 
 var lowestAddress = ranges => {
-	ranges = R.sortBy(R.tail, ranges);
 	var lowest = 0;
 	var range;
 	while(range = R.find(rangeContains(lowest), ranges)) {
